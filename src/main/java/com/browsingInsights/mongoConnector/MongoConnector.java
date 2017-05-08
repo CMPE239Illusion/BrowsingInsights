d0820e5c37534a3896b1e7e21d1a74e7424d48bc
ChandanaRao13 <chandana.rao13@gmail.com>
2017-05-07 17:31:58 -0700

    Refactoring Mongo Connector

--- src/main/java/com/browsingInsights/mongoConnector/MongoConnector.java
+++ src/main/java/com/browsingInsights/mongoConnector/MongoConnector.java
@@ -8,17 +8,18 @@ import com.mongodb.client.MongoDatabase;
  * Created by maverick on 5/7/17.
  */
 public class MongoConnector {
-    public MongoClient mongoClient;
-
+    private static MongoClient mongoClient=null;
+    
     // Function to connect to Mongo
-    public MongoDatabase connectToDB(){
-        MongoClientURI mongoClientURI = new MongoClientURI("mongodb://vaisham92:marias@ds131041.mlab.com:31041/history");
-        this.mongoClient = new MongoClient(mongoClientURI);
+    public static MongoDatabase connectToDB(){
+    	if(mongoClient==null){
+    		MongoClientURI mongoClientURI = new MongoClientURI("mongodb://vaisham92:marias@ds131041.mlab.com:31041/history");
+    		mongoClient = new MongoClient(mongoClientURI);
+    	}
         return mongoClient.getDatabase("history");
     }
-
     // Function to disconnect from Mongo
-    public void disconnectDB(){
-        this.mongoClient.close();
+    public static void disconnectDB(){
+        mongoClient.close();
     }
 }
